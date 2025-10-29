721435e4d7a14a21433205edb7ddfdde50e0540a
Christopher Weibel <christopher.weibel@gsa.gov>
2025-10-29 09:22:17 -0400

    Patch staging to support uaa v78 series

---
+++ SATS_v78/integration_test.py
@@ -0,0 +1,140 @@
+import typing
+
+from bs4 import BeautifulSoup
+import pyotp
+import requests
+
+# CSRF Element looks like <input type="hidden" name="csrf_token" value="_97d8d610c343b1cdd5386aedfcc5451d2ec32e97">
+
+class IntegrationTestClient:
+    def __init__(self, extras_url, idp_url, uaa_url, idp_name) -> None:
+        self.s = requests.Session()
+        self.extras_url = extras_url
+        self.idp_url = idp_url
+        self.uaa_url = uaa_url
+        self.idp_name = idp_name
+
+    def get_page(self, page, **kwargs) -> requests.models.Response:
+        url = self.extras_url + page
+        return self.s.get(url, **kwargs)
+
+    def post_to_page(self, page, **kwargs) -> requests.models.Response:
+        url = self.extras_url + page
+        return self.s.post(url, **kwargs)
+
+    def uaa_pick_idp(self) -> str:
+        r = self.s.get(self.uaa_url + "/login")
+        r = self.s.get(f"{self.uaa_url}/saml2/authenticate/{self.idp_name}")
+        soup = BeautifulSoup(r.text, features="html.parser")
+        saml_request = soup.find(attrs={"name": "SAMLRequest"}).attrs["value"]
+        relay_state = soup.find(attrs={"name": "RelayState"}).attrs["value"]
+        payload = dict(RelayState=relay_state, SAMLRequest=saml_request)
+        r = self.s.post(f"{self.idp_url}/profile/SAML2/POST/SSO", data=payload)
+        return r
+
+    def idp_start_log_in(self, url, csrf):
+        payload = {
+            "shib_idp_ls_exception.shib_idp_session_ss": "",
+            "shib_idp_ls_success.shib_idp_session_ss": "false",
+            "shib_idp_ls_value.shib_idp_session_ss": "",
+            "shib_idp_ls_exception.shib_idp_persistent_ss": "",
+            "shib_idp_ls_success.shib_idp_persistent_ss": "false",
+            "shib_idp_ls_value.shib_idp_persistent_ss": "",
+            "shib_idp_ls_supported": "true",
+            "_eventId_proceed": "",
+        }
+        if csrf is not None:
+            payload["csrf_token"] = csrf
+        r = self.s.post(f"{self.idp_url}{url}", data=payload)
+        return r
+
+    def idp_username_password_login(self, url, username, password, csrf):
+        payload = {
+            "j_username": username,
+            "j_password": password,
+            "_eventId_proceed": "",
+        }
+        if csrf is not None:
+            payload["csrf_token"] = csrf
+        r = self.s.post(f"{self.idp_url}{url}", data=payload)
+        return r
+
+    def idp_totp_login(self, body, totp_seed=None) -> typing.Tuple[str, bool, str]:
+        totp_updated = False
+        soup = BeautifulSoup(body, features="html.parser")
+        form = soup.find("form")
+        csrf = get_csrf_for_form(form)
+        next_url = form.attrs["action"]
+        if totp_seed is not None:
+            totp = pyotp.TOTP(totp_seed)
+        if form is not None and "barcode" in str(form):
+            totp_updated = True
+            totp_seed = soup.find("strong").string
+            totp = pyotp.TOTP(totp_seed)
+            payload = {
+                "j_tokenNumber": totp.now(),
+                "_eventId_proceed": "",
+                "state": "$state",
+            }
+            if csrf is not None:
+                payload["csrf_token"] = csrf
+            r = self.s.post(f"{self.idp_url}{next_url}", data=payload)
+            soup = BeautifulSoup(r.text, features="html.parser")
+            form = soup.find("form")
+            csrf = get_csrf_for_form(form)
+            next_url = form.attrs["action"]
+        payload = {
+            "j_tokenNumber": totp.now(),
+            "_eventId_proceed": "",
+            "state": "$state",
+        }
+        if csrf is not None:
+            payload["csrf_token"] = csrf
+        r = self.s.post(f"{self.idp_url}{next_url}", data=payload)
+        return totp_seed, totp_updated, r
+
+
+    def log_in(self, username, password, totp_seed=None) -> typing.Tuple[str, bool]:
+        """
+        log in using the shibboleth totp IDP.
+        handles registering totp if user does not have one currently
+        returns a tuple of str, bool representing the user's totp_seed, and whether a new totp was registered
+        """
+        
+        r = self.uaa_pick_idp()
+        soup = BeautifulSoup(r.text, features="html.parser")
+        form = soup.find("form")
+        next_url = form.attrs["action"]
+        csrf = get_csrf_for_form(form)
+
+        r = self.idp_start_log_in(next_url, csrf)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        form = soup.find("form")
+        next_url = form.attrs["action"]
+        csrf = get_csrf_for_form(form)
+        r = self.idp_username_password_login(next_url, username, password, csrf)
+        totp_seed, totp_updated, r = self.idp_totp_login(r.text, totp_seed)
+
+        soup = BeautifulSoup(r.text, features="html.parser")
+        saml_request = soup.find(attrs={"name": "SAMLResponse"}).attrs["value"]
+        relay_state = soup.find(attrs={"name": "RelayState"}).attrs["value"]
+        form = soup.find("form")
+        action = form.attrs["action"]
+        csrf = get_csrf_for_form(form)
+        payload = dict(RelayState=relay_state, SAMLRequest=saml_request)
+        if csrf is not None:
+            payload["csrf_token"] = csrf
+        r = self.s.post(action, data=payload)
+        r = self.s.get(self.uaa_url)
+        return totp_seed, totp_updated
+
+    def log_out(self) -> None:
+        self.s.get(f"{self.uaa_url}/logout.do")
+        self.s = requests.Session()
+
+def get_csrf_for_form(form):
+    def is_csrf_token(input):
+        return input.has_attr("name") and input.attrs["name"] == "csrf_token"
+    token_input = form.find(is_csrf_token)
+    if token_input is not None:
+        return token_input.attrs["value"]
---
+++ SATS_v78/test_shibboleth.py
@@ -0,0 +1,165 @@
+import os
+import random
+import string
+import time
+
+from bs4 import BeautifulSoup
+import pytest
+
+from .uaaclient import UAAClient
+from .integration_test import IntegrationTestClient, get_csrf_for_form
+
+
+@pytest.fixture
+def config():
+    config = {}
+    urls = {}
+    urls["uaa"] = os.environ["UAA_URL"]
+    urls["extras"] = os.environ["EXTRAS_URL"]
+    urls["idp"] = os.environ["IDP_URL"]
+    for url in urls:
+        if not urls[url][0:4] == "http":
+            urls[url] = "https://" + urls[url]
+    config["urls"] = urls
+    config["idp_name"] = os.environ["IDP_NAME"]
+    config["uaa_client"] = os.environ["UAA_USER"]
+    config["uaa_secret"] = os.environ["UAA_SECRET"]
+    return config
+
+
+@pytest.fixture
+def uaa(config):
+    uaac = UAAClient(config["urls"]["uaa"], None, verify_tls=True)
+    token = uaac._get_client_token(config["uaa_client"], config["uaa_secret"])
+    uaac.token = token
+    return uaac
+
+
+@pytest.fixture
+def user(uaa, config):
+    user = {}
+    user["name"] = (
+        "noreply+" + "".join(random.choices(string.ascii_lowercase, k=8)) + "@cloud.gov"
+    )
+    user["password"] = "".join(
+        random.choices(
+            string.ascii_lowercase + string.ascii_uppercase + string.digits, k=20
+        )
+    )
+    r = uaa.create_user(
+        user["name"],
+        "unimportant",
+        "alsounimportant",
+        user["name"],
+        password=user["password"],
+        origin="cloud.gov",
+    )
+    uaa.set_temporary_password(
+        config["uaa_client"], config["uaa_secret"], user["name"], user["password"]
+    )
+    yield user
+    uaa.delete_user(r["id"])
+
+
+@pytest.fixture
+def unauthenticated(config):
+    itc = IntegrationTestClient(
+        config["urls"]["extras"],
+        config["urls"]["idp"],
+        config["urls"]["uaa"],
+        config["idp_name"],
+    )
+    return itc
+
+
+@pytest.fixture
+def authenticated(unauthenticated, user):
+    token, changed = unauthenticated.log_in(user["name"], user["password"])
+    if changed:
+        user["token"] = token
+    return unauthenticated
+
+
+def get_csrf(page_text) -> str:
+    page = BeautifulSoup(page_text, features="html.parser")
+    csrf = page.find(attrs={"name": "_csrf_token"}).attrs["value"]
+    return csrf
+
+
+@pytest.mark.parametrize("page", ["/invite", "/change-password", "/first-login"])
+def test_unauthenticated_pages_redirect(unauthenticated, page, config):
+    r = unauthenticated.get_page(page)
+    assert r.status_code == 200
+    assert r.url == config["urls"]["uaa"] + "/login"
+
+
+def test_login_no_totp(unauthenticated, config, user):
+    # this is the happiest-path test
+    # log in to get/set our totp
+    token, changed = unauthenticated.log_in(user["name"], user["password"])
+    assert changed
+    # log out, so log in will work
+    unauthenticated.log_out()
+
+    # log in again to make sure we have the right totp
+    _, changed = unauthenticated.log_in(user["name"], user["password"], token)
+    assert not changed
+
+
+def test_no_login_with_bad_password(unauthenticated, config, user):
+    response = unauthenticated.uaa_pick_idp()
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.text, features="html.parser")
+    form = soup.find("form")
+    next_url = form.attrs["action"]
+    csrf = get_csrf_for_form(form)
+    response = unauthenticated.idp_start_log_in(next_url, csrf)
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.text, features="html.parser")
+    form = soup.find("form")
+    next_url = form.attrs["action"]
+    csrf = get_csrf_for_form(form)
+    response = unauthenticated.idp_username_password_login(next_url, user["name"], "bad-password", csrf)
+    assert response.status_code == 200
+    assert "invalid username or password" in response.text.lower()
+
+
+def test_no_login_with_bad_totp(unauthenticated, config, user):
+    # log in so we have a user with a good totp seed
+    token, changed = unauthenticated.log_in(user["name"], user["password"])
+    assert changed
+    # log out, so log in will work
+    unauthenticated.log_out()
+
+    response = unauthenticated.uaa_pick_idp()
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.text, features="html.parser")
+    form = soup.find("form")
+    next_url = form.attrs["action"]
+    csrf = get_csrf_for_form(form)
+    response = unauthenticated.idp_start_log_in(next_url, csrf)
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.text, features="html.parser")
+    form = soup.find("form")
+    next_url = form.attrs["action"]
+    csrf = get_csrf_for_form(form)
+    response = unauthenticated.idp_username_password_login(next_url, user["name"], user["password"], csrf)
+    assert response.status_code == 200
+    soup = BeautifulSoup(response.text, features="html.parser")
+    form = soup.find("form")
+    # the action will look like <url>?execution=e1s5
+    first_totp_step = int(form.attrs["action"][-1])
+    _, _, response = unauthenticated.idp_totp_login(response.text, totp_seed="asdf")
+
+    soup = BeautifulSoup(response.text, features="html.parser")
+    form = soup.find("form")
+    second_totp_step = int(form.attrs["action"][-1])
+    # the last digit of the execution value increments every time you retry the token
+    # this is kind of a silly test, but the token input UI doesn't change if you put in a bad token
+    # so this hopefully tests that we actually posted, failed the comparison, and got a response back
+    assert first_totp_step + 1 == second_totp_step
+    assert "token code from your authentication app" in response.text
